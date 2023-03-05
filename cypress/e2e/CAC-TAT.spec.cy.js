ab5e20331f090ca4230451cdc04cee1e3e6104b2
Jean P. K. C Guerra <jeanpwar@hotmail.com>
2023-03-05 17:54:26 -0300

    adicionando linha

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -236,7 +236,7 @@ it('testa a página da política de privacidade de forma independente',function(
 
   cy.visit('./src/privacy.html')
   cy.contains('h1','CAC TAT - Política de privacidade').should('be.visible')
-  
+  cy.contains('p','Talking About Testing').should('be.visible')
 
 
 })
